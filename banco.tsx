0fe4fed1a20c3b41997736eefe4264fa81d88fbe
Adriano0702 <adriano.silva.nery@hotmail.com>
2025-04-14 20:23:38 -0300

    sqliet e expo

---
+++ banco.tsx
@@ -0,0 +1,57 @@
+import react from "react";
+import { Button, View } from "react-native"
+import * as SQLite from 'expo-sqlite';
+
+
+// componentes
+
+ const Banco = ()=>{
+
+    async function criaDatabase(){
+        const db = await SQLite.openDatabaseAsync('PAM2');
+        if(db){
+            console.log('banco criado');
+        }else{
+            console.log('erro');
+        }
+        return db;
+    }
+
+    async function createTABLE(){
+        try {
+            let db = await criaDatabase();
+            await db.execAsync(`
+                PRAGMA journal_mode = WAL;
+                CREATE TABLE IF NOT EXISTS tb_usuario
+                (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);`             
+            );
+            console.log('tabela criada')
+        }catch (error) {
+            console.error('Erro ao criar tabela:', error);
+        }
+    }
+
+    async function inserirDADOS(){
+        let db = criaDatabase();
+        (await db).execAsync(`
+            PRAGMA journal_mode = WAL;
+            CREATE TABLE IF NOT EXISTS tb_usuario
+            (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);`
+        );
+    }
+
+    return(
+        <View>
+            <Button
+            title="Create Banco"
+            onPress={criaDatabase}
+            />
+            
+            <Button
+                title="inserir dados"
+                onPress={()=>{inserirDADOS()}}
+            />
+        </View>
+    )
+ }
+ export default Banco;
