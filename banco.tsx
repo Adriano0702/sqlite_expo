7d8831950239cb937f5694ad5d74d0034a390bcc
Adriano0702 <adriano.silva.nery@hotmail.com>
2025-04-14 20:54:25 -0300

    novas funções

--- banco.tsx
+++ banco.tsx
@@ -17,28 +17,35 @@ import * as SQLite from 'expo-sqlite';
         return db;
     }
 
-    async function createTABLE(){
-        try {
-            let db = await criaDatabase();
-            await db.execAsync(`
-                PRAGMA journal_mode = WAL;
-                CREATE TABLE IF NOT EXISTS tb_usuario
-                (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);`             
-            );
-            console.log('tabela criada')
-        }catch (error) {
-            console.error('Erro ao criar tabela:', error);
+    async function createTABLE() {
+        try{
+       
+        let db = await criaDatabase();
+        await db.execAsync(`
+            PRAGMA journal_mode = WAL;
+            CREATE TABLE IF NOT EXISTS tb_usuario
+            (id INTEGER PRIMARY KEY NOT NULL, nome VARCHAR);`);
+ 
+            console.log ('Tabela Criada');
+        }catch(E){
+            console.log('ERRO');
         }
     }
 
-    async function inserirDADOS(){
-        let db = criaDatabase();
-        (await db).execAsync(`
-            PRAGMA journal_mode = WAL;
-            CREATE TABLE IF NOT EXISTS tb_usuario
-            (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);`
-        );
+    async function inserirDADOS() {
+        try{
+       
+        let db = await criaDatabase();
+        await db.execAsync(`INSERT INTO tb_usuario (id,nome) VALUES (1,'Zé Ruela');`)
+           
+            console.log ('Dados Inseridos');
+        }catch(E){
+            console.log('ERRO');
+        }
     }
+
+
+   
 
     return(
         <View>
@@ -49,6 +56,11 @@ import * as SQLite from 'expo-sqlite';
             
             <Button
                 title="inserir dados"
+                onPress={()=>{createTABLE()}}
+            />
+
+            <Button
+                title="inserir"
                 onPress={()=>{inserirDADOS()}}
             />
         </View>
